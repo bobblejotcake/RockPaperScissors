0a6567b7af64f757d3a86ff01b4a06d44eb7db94
bobblejotcake <seanlimhe@gmail.com>
2026-01-28 16:56:54 +1000

    Initialize the Basic Algorithm for Rock Paper Scissors

---
+++ myscript.js
@@ -0,0 +1,125 @@
+
+console.log("We're so back");
+
+function getComputerChoice(){
+    // query random number gen
+    let computerAction = Math.random();
+    console.log(computerAction);
+    let finalAction = "";
+
+    //Check for cases
+    //If num < 0.33 = rock
+    if(computerAction < 0.33){
+        finalAction = "rock";
+    }
+    //If num >= 33 but < 66 then Scissors
+    else if(computerAction >= 0.33 && computerAction < 0.66){
+        finalAction = "scissors"
+    }
+     //Otherwise set answer to paper.
+    else{
+        finalAction = "paper";
+    }
+    
+    return finalAction;
+    
+}
+
+function getHumanChoice(){
+    return prompt("Pick rock, paper or scissors.");
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    //lowercase human response
+    let humanLower = humanChoice.toLowerCase();
+
+    let roundWinner = 10;
+
+    //Check human choice against computer
+    switch(humanLower){
+        case "rock":
+            roundWinner = rockTest(computerChoice);
+            break
+        case "scissors":
+            roundWinner = scissorTest(computerChoice);
+            break
+        case "paper":
+            roundWinner =paperTest(computerChoice);
+            break
+    }
+    // 0 represents tie, 1 represents playing losing, 2 represents player winning
+
+    //display who won
+
+    switch(roundWinner){
+        case 0:
+            roundMessage = `It's a tie! Player chose ${humanLower}. Computer chose ${computerChoice}.`
+            break
+
+        case 1:
+            roundMessage = `You lost! Player chose ${humanLower}. Computer chose ${computerChoice}.`
+            break
+
+        case 2:
+            roundMessage = `You won! Player chose ${humanLower}. Computer chose ${computerChoice}.`
+            break
+
+        case 10:
+            roundWinner = `Nope you broke something. Go back.`
+    }
+
+    console.log(roundMessage);
+
+}
+
+
+//These function assume the player selected their specific options and will return a 0,1 or 2 depending on whether they won, lost or tied against the computer
+function rockTest(computer){
+    switch (computer){
+        case "rock":
+            return 0;
+        
+        case "scissors":
+            return 2;
+
+        case "paper":
+            return 1;
+    }
+}
+
+
+function scissorTest(computer){
+    switch (computer){
+        case "rock":
+            return 1;
+        
+        case "scissors":
+            return 0;
+
+        case "paper":
+            return 2;
+    }
+}
+
+
+function paperTest(computer){
+    switch (computer){
+        case "rock":
+            return 2;
+        
+        case "scissors":
+            return 1;
+
+        case "paper":
+            return 0;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
